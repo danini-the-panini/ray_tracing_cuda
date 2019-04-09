d4a9da7230a652c7a6ff4f92af3692d4dc154b4d
Daniel Smith <jellymann@gmail.com>
2019-04-09 22:37:16 +0200

    add: sphere hit function

--- main.cu
+++ main.cu
@@ -11,7 +11,19 @@
 #define NX 200
 #define NY 100
 
+__device__ bool hit_sphere(const vec3 &center, float radius, const ray &r) {
+    vec3 oc = minus(r.origin, center);
+    float a = dot(r.direction, r.direction);
+    float b = 2.0 * dot(oc, r.direction);
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 __device__ vec3 color(const ray &r) {
+    if (hit_sphere(make_vec3(0,0,-1), 0.5, r)) {
+        return make_vec3(1,0,0);
+    }
     vec3 unit_direction = unit_vector(r.direction);
     float t = 0.5 * unit_direction.y + 1.0;
     return plus(
