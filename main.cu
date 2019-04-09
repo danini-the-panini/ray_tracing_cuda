1b9631a2eb7b322d3e5ce611d5062281b1b756d7
Daniel Smith <jellymann@gmail.com>
2019-04-09 22:57:32 +0200

    add: surface normals to sphere

--- main.cu
+++ main.cu
@@ -11,21 +11,27 @@
 #define NX 200
 #define NY 100
 
-__device__ bool hit_sphere(const vec3 &center, float radius, const ray &r) {
+__device__ float hit_sphere(const vec3 &center, float radius, const ray &r) {
     vec3 oc = minus(r.origin, center);
     float a = dot(r.direction, r.direction);
     float b = 2.0 * dot(oc, r.direction);
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 __device__ vec3 color(const ray &r) {
-    if (hit_sphere(make_vec3(0,0,-1), 0.5, r)) {
-        return make_vec3(1,0,0);
+    float t = hit_sphere(make_vec3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(minus(point_at_parameter(r, t), make_vec3(0,0,-1)));
+        return times(0.5, make_vec3(N.x+1, N.y+1, N.z+1));
     }
     vec3 unit_direction = unit_vector(r.direction);
-    float t = 0.5 * unit_direction.y + 1.0;
+    t = 0.5 * unit_direction.y + 1.0;
     return plus(
         times(1.0-t, make_vec3(1.0, 1.0, 1.0)),
         times(    t, make_vec3(0.5, 0.7, 1.0))
