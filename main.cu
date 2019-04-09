ce4dbc189488e89603550a2b71d5fe072a1b601b
Daniel Smith <jellymann@gmail.com>
2019-04-10 01:46:45 +0200

    add: multiple random samples

--- main.cu
+++ main.cu
@@ -6,12 +6,24 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#include <curand.h>
+#include <curand_kernel.h>
+
+#include <cub/cub.cuh>
+
 #include "vec3.h"
 #include "ray.h"
 #include "hitable_utils.h"
 
 #define NX 200
 #define NY 100
+#define NS 100
+
+__global__ void setup_kernel(curandState * state, unsigned long seed)
+{
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    curand_init((seed<<20)+id, 0, 0, &state[id]);
+}
 
 __device__ vec3 color(const ray &r, hitable *world) {
     hit_record rec;
@@ -28,7 +40,13 @@ __device__ vec3 color(const ray &r, hitable *world) {
     }
 }
 
-__global__ void kernel(int nx, int ny, hitable *world, unsigned char *out) {
+__global__ void kernel(curandState* global_state, int nx, int ny, hitable *world, unsigned char *out) {
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    curandState local_state = global_state[id];
+
+    typedef cub::BlockReduce<vec3, NS> BlockReduce;
+    __shared__ typename BlockReduce::TempStorage temp_storage;
+
     vec3 lower_left_corner = make_vec3(-2.0, -1.0, -1.0);
     vec3 horizontal = make_vec3(4.0, 0.0, 0.0);
     vec3 vertical = make_vec3(0.0, 2.0, 0.0);
@@ -38,18 +56,28 @@ __global__ void kernel(int nx, int ny, hitable *world, unsigned char *out) {
     int j = ny-n/nx-1;
     int i = n%nx;
 
-    float u = float(i) / float(nx);
-    float v = float(j) / float(ny);
+    float u = (float(i) + curand_uniform(&local_state)) / float(nx);
+    float v = (float(j) + curand_uniform(&local_state)) / float(ny);
 
     ray r = make_ray(origin, plus(lower_left_corner, plus(times(u, horizontal), times(v, vertical))));
     vec3 col = color(r, world);
+    div(col, float(NS));
 
-    out[n*3+0] = int(255.99*col.r);
-    out[n*3+1] = int(255.99*col.g);
-    out[n*3+2] = int(255.99*col.b);
+    vec3 final_col = BlockReduce(temp_storage).Reduce(col, plus);
+
+    if (threadIdx.x == 0) {
+        out[n*3+0] = int(255.99*final_col.r);
+        out[n*3+1] = int(255.99*final_col.g);
+        out[n*3+2] = int(255.99*final_col.b);
+    }
 }
 
 int main(void) {
+    curandState* device_states;
+    cudaMalloc(&device_states, NX*NY*NS*sizeof(curandState));
+
+    setup_kernel<<<NX*NY,NS>>>(device_states, time(NULL));
+
     size_t BUFFER_SIZE = sizeof(unsigned char)*NX*NY*3;
 
     printf("P3\n%d %d\n255\n", NX, NY);
@@ -63,7 +91,7 @@ int main(void) {
     unsigned char *d_out; // device output
     cudaMalloc(&d_out, BUFFER_SIZE);
 
-    kernel<<<NX*NY,1>>>(NX, NY, world, d_out);
+    kernel<<<NX*NY,NS>>>(device_states, NX, NY, world, d_out);
 
     cudaMemcpy(out, d_out, BUFFER_SIZE, cudaMemcpyDeviceToHost);
 
